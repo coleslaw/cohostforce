b7c7b7b3bc5b783b5114696ac212ef70db68ec6e
Luzivanlt <gokuhdvn3@gmail.com>
2023-03-04 10:53:56 +0700

    django app to display user profiles

--- cohostforces/settings.py
+++ cohostforces/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'online_site',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ online_site/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from online_site.models import *
+
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(ContestResult)
