8b514a6c71510f62e6c4fe0e7037f9a6c9ed0a5a
TrKNguyen <nguyenusaco@gmail.com>
2023-05-13 10:08:10 +0700

    add

--- cohostforces/settings.py
+++ cohostforces/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,4 +138,6 @@ ELASTICSEARCH_DSL={
         'hosts': 'https://elastic:xhBqmy8XqOYQSZYjspBVsnPK@daa681.es.us-east-1.aws.found.io'
     },
 }
-ALLOWED_HOSTS = ['cohostforce.herokuapp.com']
+#ALLOWED_HOSTS = ['cohostforce.herokuapp.com']
+
+django_heroku.settings(locals())
